9ab244c6b6217d0dbd094197d53d29f8d3818f84
Alex <7alex7li@gmail.com>
2019-11-02 17:37:42 -0400

    Database kinda works

--- app/src/main/java/com/example/scheduler/MainActivity.java
+++ app/src/main/java/com/example/scheduler/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        db = FirebaseDatabase.getInstance().getReference();
+//        new Database().addCourse();
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
