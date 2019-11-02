37278300a1e920049878c8c7d47ca20258eb0c6b
Noah Miller <35278226+DonnerPrinz@users.noreply.github.com>
2019-11-02 16:18:30 -0400

    Course

---
+++ app/src/main/java/com/example/scheduler/Course.java
@@ -0,0 +1,32 @@
+package com.example.scheduler;
+
+import java.util.HashSet;
+
+public class Course {
+    private String courseNum;
+    private int creditHours;
+    private String informalName;
+    private HashSet<String> prereqs;
+
+    public Course(String courseNum){
+        this.courseNum = courseNum;
+        this.creditHours = 0;
+        this.informalName = "";
+        this.prereqs = new HashSet<String>();
+    }
+
+    public String getCourseNum() {
+        return this.courseNum;
+    }
+    public int getcreditHours() {
+        return this.creditHours;
+    }
+    public String getinformalName() { return this.informalName; }
+    public HashSet<String> getPrereqs() { return this.prereqs; }
+
+    public void setCourseNum(String courseNum) {
+        this.courseNum = courseNum;
+    }
+    public void setCreditHours(int crhrs) { this.creditHours = crhrs; }
+    public void setInformalName (String infName) { this.informalName = infName; }
+}
